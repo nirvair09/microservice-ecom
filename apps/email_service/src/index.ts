a2848b167ab42ea37f36e07ddab36ebc9389ecb0
nirvair09 <chandradeep262003@gmail.com>
2025-11-25 12:14:36 +0530

    feat: add email service to send welcome and order confirmation emails for user and order events.

--- apps/email_service/src/index.ts
+++ apps/email_service/src/index.ts
@@ -1,9 +1,45 @@
 import { createConsumer, createKafkaClient } from "@repo/kafka";
-
+import sendMail from "./utils/mailer";
 const kafka = createKafkaClient("email-service");
 const consumer=createConsumer(kafka,"email-service");
 
 const startEmailService=async()=>{
+  try {
+    await consumer.connect();
+    await consumer.subscribe([
+      { topicName:"user.created",
+        topicHandler:async(message)=>{
+          const {email,username}=message;
+          if(email){
+            await sendMail({
+              email,
+              subject:"Welcome to E-commerce",
+              text: `Welcome ${username}. You account has been created!`,
+            })
+          }
+        }
+
+      },{
+
+        topicName: "order.created",
+        topicHandler: async (message) => {
+          const { email, amount, status } = message.value;
+
+          if (email) {
+            await sendMail({
+              email,
+              subject: "Order has been created",
+              text: `Hello! Your order: Amount: ${amount/100}, Status: ${status}`,
+            });
+          }
+        },
+
+      },
+    ]);
+    
+  } catch (error) {
+    console.log(error);
+  }
 
 };
 
